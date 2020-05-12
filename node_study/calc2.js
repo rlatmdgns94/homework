b9d60be7b1d03fb5f18d6e07036e9d67e08ddb7a
kimseunghun <rlatmdgns94@naver.com>
2020-05-12 22:21:17 +0900

    node

---
+++ node_study/calc2.js
@@ -0,0 +1,7 @@
+var calc = {}; //변수 선언 , 객체 할당
+
+calc.add = function (a, b) {
+  return a + b;
+}; //calc객체 속성 정의, 함수 할당 //익명함수
+
+module.exports = calc;
---
+++ node_study/ch02_test6.js
@@ -0,0 +1,6 @@
+var calc2 = require("./calc2");
+
+//모듈로 분리 후
+var result = calc2.add(20, 20);
+
+console.log(result);
